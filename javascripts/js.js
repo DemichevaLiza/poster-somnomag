22a8ba7563b980cabf09a2af54be856e515b0c09
Liza Demicheva <90695376+DemichevaLiza@users.noreply.github.com>
2022-03-11 12:20:57 +0300

    1th

---
+++ javascripts/js.js
@@ -0,0 +1,253 @@
+  $(document).ready(function(){
+    $('.switcha').click(function(){
+    $(this).toggleClass('switcha-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.switchb').click(function(){
+    $(this).toggleClass('switchb-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.switchc').click(function(){
+    $(this).toggleClass('switchc-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.switchd').click(function(){
+    $(this).toggleClass('switchd-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.switche').click(function(){
+    $(this).toggleClass('switche-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.switchf').click(function(){
+    $(this).toggleClass('switchf-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.img1').click(function(){
+    $(this).toggleClass('img1-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.img2').click(function(){
+    $(this).toggleClass('img2-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.img3').click(function(){
+    $(this).toggleClass('img3-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.img4').click(function(){
+    $(this).toggleClass('img4-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.img5').click(function(){
+    $(this).toggleClass('img5-on');
+  });
+  });
+
+  $(document).ready(function(){
+    $('.img6').click(function(){
+    $(this).toggleClass('img6-on');
+  });
+  });
+
+ $(document).ready(function(){
+    $('.sectionswa').click(function(){
+    $('.sectionmain').css('display','block');
+    $('.sectionmain').css('display','none');
+});
+});
+
+
+ $(document).ready(function(){
+    $('.sectionswa').click(function(){
+    $('.section1').css('display','none');
+    $('.section1').css('display','block');
+
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswa').click(function(){
+   $('.sectionballs').css('display','none');
+   $('.sectionballs').css('display','block');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswa').click(function(){
+   $('.sectionswb').css('display','block');
+   $('.sectionswb').css('display','none');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswa').click(function(){
+   $('.sectionswc').css('display','block');
+   $('.sectionswc').css('display','none');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswa').click(function(){
+   $('.sectionswd').css('display','block');
+   $('.sectionswd').css('display','none');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswa').click(function(){
+   $('.sectionswe').css('display','block');
+   $('.sectionswe').css('display','none');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswa').click(function(){
+   $('.sectionswf').css('display','block');
+   $('.sectionswf').css('display','none');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswb').click(function(){
+   $('body').css('background-color','#313F86');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswb').click(function(){
+   $('.sectionmain').css('display','block');
+   $('.sectionmain').css('display','none');
+});
+});
+
+
+$(document).ready(function(){
+   $('.sectionswb').click(function(){
+   $('.section2').css('display','none');
+   $('.section2').css('display','block');
+
+});
+});
+
+
+$(document).ready(function(){
+  $('.sectionswb').click(function(){
+  $('.sectionswa').css('display','block');
+  $('.sectionswa').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswb').click(function(){
+  $('.sectionswc').css('display','block');
+  $('.sectionswc').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswb').click(function(){
+  $('.sectionswd').css('display','block');
+  $('.sectionswd').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswb').click(function(){
+  $('.sectionswe').css('display','block');
+  $('.sectionswe').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswb').click(function(){
+  $('.sectionswf').css('display','block');
+  $('.sectionswf').css('display','none');
+});
+});
+
+
+$(document).ready(function(){
+   $('.sectionswc').click(function(){
+   $('body').css('background-color','#313F86');
+});
+});
+
+$(document).ready(function(){
+   $('.sectionswc').click(function(){
+   $('.sectionmain').css('display','block');
+   $('.sectionmain').css('display','none');
+});
+});
+
+
+$(document).ready(function(){
+   $('.sectionswc').click(function(){
+   $('.section3').css('display','none');
+   $('.section3').css('display','block');
+
+});
+});
+
+
+$(document).ready(function(){
+  $('.sectionswc').click(function(){
+  $('.sectionswa').css('display','block');
+  $('.sectionswa').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswc').click(function(){
+  $('.sectionswb').css('display','block');
+  $('.sectionswb').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswc').click(function(){
+  $('.sectionswd').css('display','block');
+  $('.sectionswd').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswc').click(function(){
+  $('.sectionswe').css('display','block');
+  $('.sectionswe').css('display','none');
+});
+});
+
+$(document).ready(function(){
+  $('.sectionswc').click(function(){
+  $('.sectionswf').css('display','block');
+  $('.sectionswf').css('display','none');
+});
+});
+
+
+ $(document).ready(function(){
+   $('.ball').click(function(){
+    $(this).toggleClass('ball-on');
+});
+});
